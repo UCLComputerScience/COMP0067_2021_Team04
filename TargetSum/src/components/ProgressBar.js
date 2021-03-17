fb7e3d1ae85955ee8ce9cdefcd707e659010d3fe
Shaun <zcfabse@ucl.ac.uk>
2021-03-17 19:56:03 +0000

    nav

--- TargetSum/src/components/ProgressBar.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { ProgressBar, Colors } from 'react-native-paper';
-
-const Progress = () => (
-  <ProgressBar progress={0.65} color={Colors.red800} />
-);
-
-export default Progress;

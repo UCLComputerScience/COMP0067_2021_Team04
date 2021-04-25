34f1caff6ff3568a92e591aaf0e64c8bff82c0d0
Andrew <chengoconnell@gmail.com>
2021-04-25 08:45:40 +0100

    challenges

--- backend/sandbox.js
+++ backend/sandbox.js
@@ -1,2 +1,15 @@
-var date = new Date();
-console.log(date.toISOString().slice(0,10))
+
+
+function parseISOString(s) {
+    var b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
+
+// date = parseISOString(date)
+// console.log(parseISOString(date))
+
+var myVariable = "28 Aug 2014"
+var makeDate = new Date();
+makeDate = new Date(makeDate.setMonth(makeDate.getMonth() - 1));
+
+console.log(makeDate.toISOString())

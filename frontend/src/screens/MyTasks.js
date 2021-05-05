45dbb1e2b3d91d855cbdd9c00231f92786bd8049
Shaun <zcfasbe@ucl.ac.uk>
2021-05-06 00:03:51 +0100

    white my task page

--- frontend/src/screens/MyTasks.js
+++ frontend/src/screens/MyTasks.js
@@ -105,7 +105,7 @@ const Tasks = ({navigation, route}) => {
     }
     
       return (
-        <ScrollView>
+        <ScrollView style={styles.container}>
       <Text style={styles.statisticTitle}>Your teacher has set you the following tasks:</Text>
       {allTasks.map(TaskComp)}
       {/* {TaskComp('Complete 8 levels of indermediate 6x tables', 0.3, '5d 2hrs')}
@@ -123,11 +123,7 @@ const Tasks = ({navigation, route}) => {
 
 const styles = StyleSheet.create ({
     container: {
-        flex: 1,
         backgroundColor: 'white',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
     },
     timeRemainingText: {
         flex: 1,
